e357d76f9513129bfbc5c4eb10a73d95cc7840d7
Andrew Roth <andrewroth@gmail.com>
2010-01-28 11:53:45 -0500

    use proper dependency for the god gem

--- lib/moonshine/manifest/rails/god.rb
+++ lib/moonshine/manifest/rails/god.rb
@@ -4,9 +4,7 @@ module Moonshine::Manifest::Rails::God
   # to <tt>/etc/god/god.rb</tt>.  Adds a config file in 
   # <tt>/etc/god/apps/<application>.yml</tt> if :delayed_job is set to true in
   # moonshine.yml
-  def god_delayed_job
-    STDOUT.puts "fgh god_delayed_job"
-    STDOUT.flush
+  def god
     file '/etc/god', :ensure => :directory
     file '/etc/god/apps', :ensure => :directory
     file '/etc/god/god.rb',
@@ -17,31 +15,19 @@ module Moonshine::Manifest::Rails::God
         :ensure => :present,
         :content => template(File.join(File.dirname(__FILE__), 'templates', 'god_app.yml.erb'))
     end
-  end
-
-  # Install the <tt>god</tt> rubygem and dependencies
-  def god_gem
-    STDOUT.puts "fgh god_gem"
-    STDOUT.flush
-    gem('god')
+    god_start
   end
 
   def god_start
-    STDOUT.puts "fgh god_start"
-    STDOUT.flush
-    exec "god_start", :command => "sudo god -c /etc/god/god.rb start"
+    gem 'god', :before => exec("god_start", :command => "sudo god -c /etc/god/god.rb start")
   end
 
   def god_stop
-    STDOUT.puts "fgh god_stop"
-    STDOUT.flush
     exec "god_stop", :command => "sudo god quit",
       :onlyif => "test `ps aux | grep '/usr/bin/ruby /usr/bin/god` -eq 0"
   end
 
   def god_restart
-    STDOUT.puts "fgh god_restart"
-    STDOUT.flush
     god_stop
     god_start
   end
