6c42c60abc3d5c05d63e86a571e733876b62cdc2
Andrew Roth <andrewroth@gmail.com>
2010-02-09 14:25:03 -0500

    revert rubygems include, it's redundant and didn't solve the problem

--- lib/rake_helper.rb
+++ lib/rake_helper.rb
@@ -69,7 +69,6 @@ end
 
 def prepare_for_sql
   unless defined?(ActiveRecord) && @sql
-    require "rubygems"
     require "active_record"
     root_config
     ActiveRecord::Base.establish_connection root_config.merge('database' => '')
