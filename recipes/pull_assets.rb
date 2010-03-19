8aacba34b9c6bb2c8ba068ba165c9e1ffdbea9ef
Andrew Roth <andrewroth@gmail.com>
2010-03-19 12:00:25 -0400

    tweak pull_assets script to actually work right

--- recipes/pull_assets.rb
+++ recipes/pull_assets.rb
@@ -1,15 +1,18 @@
 def pull_asset(remote, local)
-  run "tar -czf #{remote}.tar.gz #{remote}"
+  basename = File.basename(remote)
+  dirname = File.dirname(remote)
+  run "cd #{dirname} && tar -czf #{basename}.tar.gz #{basename}"
   download "#{remote}.tar.gz", "#{local}.tar.gz"
-  puts "tar xfz #{local}.tar.gz"
-  system "tar xfz #{local}.tar.gz"
+  cmd = "cd #{File.dirname(local)} && tar xfz #{local}.tar.gz"
+  puts cmd
+  system cmd
 end
 
 task :pull_assets do
+  role :app, 'pat.powertochange.org'
   set :user, 'deploy'
   set :host, 'pat.powertochange.org'
 
   pull_asset '/var/www/pat.powertochange.org/shared/public/event_groups',
     '/var/www/pat.powertochange.org/shared/public/event_groups'
 end
-
