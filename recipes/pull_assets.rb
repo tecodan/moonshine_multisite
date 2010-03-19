bb0d7e26b5be3fb878db22304fabdf88a4c24da5
Andrew Roth <andrewroth@gmail.com>
2010-03-19 12:18:45 -0400

    proper pull assets task with all assets

--- recipes/pull_assets.rb
+++ recipes/pull_assets.rb
@@ -13,6 +13,21 @@ task :pull_assets do
   set :user, 'deploy'
   set :host, 'pat.powertochange.org'
 
+  # pat
   pull_asset '/var/www/pat.powertochange.org/shared/public/event_groups',
     '/var/www/pat.powertochange.org/shared/public/event_groups'
+  pull_asset '/var/www/dev.pat.powertochange.org/shared/public/event_groups',
+    '/var/www/pat.dev.powertochange.org/shared/public/event_groups'
+  # mpdtool
+  pull_asset '/var/www/mpdtool.powertochange.org/shared/public/mpd_letter_images',
+    '/var/www/mpdtool.powertochange.org/shared/public/mpd_letter_images'
+  pull_asset '/var/www/dev.mpdtool.powertochange.org/shared/public/mpd_letter_images',
+    '/var/www/mpdtool.dev.powertochange.org/shared/public/mpd_letter_images'
+  # pulse
+  pull_asset '/var/www/pulse.campusforchrist.org/shared/public/emu.profile_pictures',
+    '/var/www/pulse.campusforchrist.org/shared/public/emu.profile_pictures'
+  pull_asset '/var/www/moose.campusforchrist.org/shared/public/emu_dev.profile_pictures',
+    '/var/www/moose.campusforchrist.org/shared/public/emu_dev.profile_pictures'
+  pull_asset '/var/www/emu.campusforchrist.org/shared/public/emu_stage.profile_pictures',
+    '/var/www/emu.campusforchrist.org/shared/public/emu_stage.profile_pictures'
 end
